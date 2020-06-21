a8ac129a4470784a8baf9f6f149b809cb39e156e
kittisak_sri <kittisak_sri@truecorp.co.th>
2020-06-21 11:41:33 +0700

    Remove old code that calculate sla and use new code from sla module

--- TQC_report.py
+++ TQC_report.py
@@ -23,6 +23,7 @@ Results:
 
 ็็Version: 0.1 - initial version
          0.5 - update sql and add calculation
+         0.6 - move SLA calculation to mobule (support default and CPC project group)
 Auther: Kittisak Srijankul (kittisak_sri@truecorp.co.th)
 """
 
@@ -32,6 +33,7 @@ from business_duration import businessDuration
 import holidays as pyholidays
 from datetime import time,date,datetime
 import math
+import sla
 
 
 #Business open hour must be in standard python time format-Hour,Min,Sec
@@ -315,27 +317,31 @@ for index, row in df.iterrows():
             defect_fixed_assigned_duration = 0
     defect_fixed_assigned_list.append(defect_fixed_assigned_duration)
 
-    sla_status = calculate_sla()
-
     # calculate SLA
-    sla_status = ''
-    if (current_severity_name == None):
-        current_severity_name = current_priority_name
-
-    # Default SLA
-    if (current_severity_name == 'Low' or current_severity_name == None ):
-        sla_status = 'Low'
-    elif (current_main_defect_status == 'Cancelled'):
-        sla_status = 'Cancelled'
-    elif (current_severity_name == 'Critical' and defect_fixed_new_duration > 4):
-        sla_status = 'N'
-    elif (current_severity_name == 'High' and defect_fixed_new_duration > 8):
-        sla_status = 'N'
-    elif (current_severity_name == 'Medium' and defect_fixed_new_duration > 16):
-        sla_status = 'N'
+    if current_project_name.startswith('CPC'):
+        sla_status = sla.calculate_sla(project_group='CPC',severity_name=current_severity_name,priority_name=current_priority_name,defect_status=current_main_defect_status,defect_fixed_new_duration=defect_fixed_new_duration)
     else:
-        #print("severity: {}".format(current_severity_name)) 
-        sla_status = 'Y'
+        sla_status = sla.calculate_sla(severity_name=current_severity_name,priority_name=current_priority_name,defect_status=current_main_defect_status,defect_fixed_new_duration=defect_fixed_new_duration)
+    
+    # # calculate SLA
+    # sla_status = ''
+    # if (current_severity_name == None):
+    #     current_severity_name = current_priority_name
+
+    # # Default SLA
+    # if (current_severity_name == 'Low' or current_severity_name == None ):
+    #     sla_status = 'Low'
+    # elif (current_main_defect_status == 'Cancelled'):
+    #     sla_status = 'Cancelled'
+    # elif (current_severity_name == 'Critical' and defect_fixed_new_duration > 4):
+    #     sla_status = 'N'
+    # elif (current_severity_name == 'High' and defect_fixed_new_duration > 8):
+    #     sla_status = 'N'
+    # elif (current_severity_name == 'Medium' and defect_fixed_new_duration > 16):
+    #     sla_status = 'N'
+    # else:
+    #     #print("severity: {}".format(current_severity_name)) 
+    #     sla_status = 'Y'
 
     meet_sla_list.append(sla_status)
     # if (defect_age_duration > 0 and defect_age_duration <= 4):
@@ -395,53 +401,3 @@ df.to_csv("TQC_query_results_"+str(datetime.now().strftime("%Y-%m-%d %H%M%S"))+"
 
 
 
-def calculate_sla(project_group,severity_name,priority_name,defect_status,defect_fixed_new_duration):
-    """calculate the SLA and check whether it meet SLA criteria or not
-
-    Parameters
-    ----------
-    project_group : str
-        The specific project group (default is blank).
-    severity_name : str
-        The severity name (Critical,High,Medium).
-    priority_name : str
-        The priority name will be used if the severity name is none.
-    defect_status : str
-        The defect status (Open,Fixed,Closed,Cancelled)
-    defect_fixed_new_duration : int
-        The defect duration from new status to fixed status
-
-    Returns
-    -------
-    str
-        Status: Y or N
-
-    """
-    sla_status = ''
-    critical_point = 4      # default value of critical point
-    high_point = 8          # default value of high point
-    medium_point = 16       # default value of medium point
-
-    # specific value for CPC project group
-    if (project_group == 'CPC'):   
-        critical_point = 24
-        high_point = 48
-        medium_point = 120
-
-    if (severity_name == None):
-        severity_name = priority_name
-
-    if (severity_name == 'Low' or severity_name == None ):
-        sla_status = 'Low'
-    elif (defect_status == 'Cancelled'):
-        sla_status = 'Cancelled'
-    elif (severity_name == 'Critical' and defect_fixed_new_duration > critical_point):
-        sla_status = 'N'
-    elif (severity_name == 'High' and defect_fixed_new_duration > high_point):
-        sla_status = 'N'
-    elif (severity_name == 'Medium' and defect_fixed_new_duration > medium_point):
-        sla_status = 'N'
-    else:
-        #print("severity: {}".format(severity_name)) 
-        sla_status = 'Y'
-    return sla_status
